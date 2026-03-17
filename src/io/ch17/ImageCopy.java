7889651181212b244b5b349649c38e66ba8d71f0
wing853 <qwqhsk@naver.com>
2026-03-17 17:38:47 +0900

    파일 입출력 실습

--- src/io/ch17/ImageCopy.java
+++ src/io/ch17/ImageCopy.java
@@ -8,14 +8,19 @@ public class ImageCopy {
     public static void main(String[] args) {
         // abc.png파일을 읽어서
         // abc2.png파일을 만들기
+        String sourceFile = "abc.png";
+        String destFile = "E:\\_work_java\\abc2.png";
+        long startTime = System.currentTimeMillis();
 
-        try (FileInputStream fis = new FileInputStream("abc.png");
-             FileOutputStream fos = new FileOutputStream("abc2.png")) {
+        try (FileInputStream fis = new FileInputStream(sourceFile);
+             FileOutputStream fos = new FileOutputStream(destFile)) {
             int data;
-            while((data = fis.read()) != -1){
-                fos.write((char)data);
+            while ((data = fis.read()) != -1) {
+                fos.write((char) data);
             }
-            System.out.println(System.currentTimeMillis());
+            long endTime = System.currentTimeMillis();
+            System.out.println("파일 저장 완료");
+            System.out.println("소요시간: " + (endTime - startTime) + "ms");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
